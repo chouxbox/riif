a9557c5432b080883811f53d1223e240dfd219a3
Vicki <vm2355@columbia.edu>
2014-08-12 17:59:33 -0700

    klass to CLASS in labels test

--- lib/riif/dsl/trns.rb
+++ lib/riif/dsl/trns.rb
@@ -37,8 +37,8 @@ module Riif::DSL
 
     def headers
       [
-        ["!#{START_COLUMN}"].concat(HEADER_COLUMNS.map! { |x| x.upcase == 'KLASS' ? 'CLASS' : x }),
-        ["!SPL"].concat(Spl::HEADER_COLUMNS.map! { |x| x.upcase == 'KLASS' ? 'CLASS' : x }),
+        ["!#{START_COLUMN}"].concat(HEADER_COLUMNS.map! { |x| x == :klass ? 'CLASS' : x.upcase }),
+        ["!SPL"].concat(Spl::HEADER_COLUMNS.map! { |x| x == :klass ? 'CLASS' : x.upcase }),
         ["!#{END_COLUMN}"]
       ]
     end
